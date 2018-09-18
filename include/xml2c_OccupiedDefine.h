64f4188557e7d7f11d0f08c6fbade0dd954855fa
weng760110 <30426705+weng760110@users.noreply.github.com>
2018-09-19 08:32:35 +0900

    Update xml2c_OccupiedDefine.h

--- include/xml2c_OccupiedDefine.h
+++ include/xml2c_OccupiedDefine.h
@@ -42,21 +42,37 @@ FUNC.EVN1.T4.RETURN
 #define XC_FUNC_OBJ_COUNT				(128)
 
 
-typedef struct _XC_DEFINE_DEPTH_T {
-	char name[XC_DEFINE_DEPTH_TREE_DEPTH][XC_DEFINE_DEPTH_NAME_LEN];
-}XC_DEFINE_DEPTH_T, *P_XC_DEFINE_DEPTH_T;
-
-typedef struct _XC_TREE {
-	int count;
-	P_XC_DEFINE_DEPTH_T dep[XC_DEFINE_DEPTH_COUNT];
-}XC_TREE_T, *P_XC_TREE_T;
-
 enum XC_CLASS_T {
 	XC_ENV = 1,
 	XC_OBJ,
 	XC_EVN,
 	XC_T
 };
+
+enum T3_CLASS_T {
+	T3_SET = 1,
+	T3_CALL,
+	T3_IF,
+	T3_SWITCH,
+	T3_LOOP,
+	T3_UNKOWN = 99
+};
+
+enum OBJ_TYPE_T {
+	OBJ_TYPE_INT = 1,
+	OBJ_TYPE_CHAR,
+	OBJ_TYPE_LONG,
+	OBJ_TYPE_UNKOWN = 99
+};
+
+/* P: Param G: global L: local */
+enum OBJ_PGL_T {
+	OBJ_PGL_P=1,
+	OBJ_PGL_G,
+	OBJ_PGL_L,
+	OBJ_PGL_UNKOWN = 99
+};
+
 
 /*
 XC_DEFINE_LEVEL_T  g_xcDefineDepthTree[XC_DEFINE_LEVEL_COUNT] = 
@@ -161,7 +177,7 @@ typedef struct _SWITCH_CASE_T {
 typedef struct _SWITCH_T {
 	char name[XC_DEFINE_ITEM_LEN];
 	SWITCH_CASE_T swCase[32];
-} SWITCH_T, *P_SWITH_T;
+} SWITCH_T, *P_SWITCH_T;
 typedef struct _CONDITION_LOOP_T {
 	char text[XC_DEFINE_ITEM_LEN];
 	char equal[4];
@@ -169,18 +185,30 @@ typedef struct _CONDITION_LOOP_T {
 typedef struct _LOOP_T {
 	char name[XC_DEFINE_ITEM_LEN];
 	CONDITION_LOOP_T loopCond;
-} LOOP_T;
+} LOOP_T, *P_LOOP_T;
 typedef struct _SET_T {
+	char from[XC_DEFINE_ITEM_LEN];
+	char to[XC_DEFINE_ITEM_LEN];
 	char name[XC_DEFINE_ITEM_LEN];
 	char text[XC_DEFINE_ITEM_LEN];
-} SET_T;
+} SET_T, *P_SET_T;
 typedef struct _T3_T {
-	EVN_CALL_T progCallFunc;
-	EVN_IF_T progIF;
-	EVN_SWITCH_T progSwitch;
-	EVN_LOOP_T progLoop;
-	EVN_SET_T progSet;
+	P_CALL_T pCall;
+	P_IF_T pIf;
+	P_SWITCH_T pSwitch;
+	P_LOOP_T pLoop;
+	P_SET_T pSet;
 } T3_T, *P_T3_T;
+
+typedef struct _XC_DEFINE_DEPTH_T {
+	char name[XC_DEFINE_DEPTH_TREE_DEPTH][XC_DEFINE_DEPTH_NAME_LEN];
+	T3_T t3;
+}XC_DEFINE_DEPTH_T, *P_XC_DEFINE_DEPTH_T;
+
+typedef struct _XC_TREE {
+	int count;
+	P_XC_DEFINE_DEPTH_T dep[XC_DEFINE_DEPTH_COUNT];
+}XC_TREE_T, *P_XC_TREE_T;
 
 
 
