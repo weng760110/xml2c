2d3be3ec3c09a4d49a92a49b217cfadc1b376835
weng760110 <30426705+weng760110@users.noreply.github.com>
2018-09-25 09:06:19 +0900

    Update xcT3Set.c

--- xcT3Set.c
+++ xcT3Set.c
@@ -6,39 +6,63 @@
 #include <libxml/parser.h>
 
 #include "xml2c_OccupiedDefine.h"
+#include "pub_xml_api.h"
+#include "pub_print.h"
 
 
 extern void die(char *msg);
 extern int _getDepthTree (xmlNodePtr cur, P_XC_TREE_T pxcTree, int depth);
 enum OBJ_TYPE_T _getObjType(char* strType);
 
-extern XC_TREE_T g_ThrdTree;
+extern XC_TREE_T* g_pThrdTree;
 extern char g_T3IndexList[65536];
 extern P_OBJ_T g_pThrdObj;
 
 
 int printT3Set(int index)
 {
-	P_SET_T pSet = g_ThrdTree.dep[index]->t3.pSet;
+	P_SET_T pSet = g_pThrdTree->dep[index]->t3.pSet;
 
 	enum OBJ_TYPE_T enumType = OBJ_TYPE_UNKOWN;
+	enum OBJ_PGL_T enumPGLfrom = OBJ_PGL_UNKOWN;
+	enum OBJ_PGL_T enumPGLto = OBJ_PGL_UNKOWN;
+	P_VALUE_T pVALfrom = NULL;
+	P_VALUE_T pVALto = NULL;
 
 	enumPGLfrom = _getObjPGL(pSet->from);
-	enumPGLto = _getObjPGL(pSet->to);
 
 	if (enumPGLfrom == OBJ_PGL_P) {
-		
-
+		pVALfrom = _getObjVal(g_pThrdObj, OBJ_PGL_P, pSet->name, strlen(pSet->name));
 	}
 	else if (enumPGLfrom == OBJ_PGL_G) {
+		pVALfrom = _getObjVal(g_pThrdObj, OBJ_PGL_G, pSet->name, strlen(pSet->name));
 	}
 	else if (enumPGLfrom == OBJ_PGL_L) {
+		pVALfrom = _getObjVal(g_pThrdObj, OBJ_PGL_L, pSet->name, strlen(pSet->name));
 	}
 	else return -1;
-	enumType = _getObjType();
-	if () {
+
+	enumPGLto = _getObjPGL(pSet->to);
+	if (enumPGLto == OBJ_PGL_P) {
+		pVALto = _getObjVal(g_pThrdObj, OBJ_PGL_P, pSet->name, strlen(pSet->name));
 	}
-	printf("\t%s = %s\n", pSet->to, pSet->from);
+	else if (enumPGLto == OBJ_PGL_G) {
+		pVALto = _getObjVal(g_pThrdObj, OBJ_PGL_G, pSet->name, strlen(pSet->name));
+	}
+	else if (enumPGLto == OBJ_PGL_L) {
+		pVALto = _getObjVal(g_pThrdObj, OBJ_PGL_L, pSet->name, strlen(pSet->name));
+	}
+	else return -1;
+
+	enumType = _getObjType(pVALto->type);
+	if (enumType == OBJ_TYPE_INT) {
+		if (pSet->to[0] == 0) {
+			PRT_INT_VAL(pVALto->name, atoi(pSet->text), 1, 1);
+		}
+		else {
+			PRT_INT_NAME(pVALto->name, pVALfrom->name, 1, 1);
+		}
+	}
 }
 
 int _T3Set(xmlNodePtr cur, int index)
@@ -72,7 +96,7 @@ int _T3Set(xmlNodePtr cur, int index)
 	}
 	
 	P_SET_T pSet = (P_SET_T)malloc(sizeof(SET_T));
-	g_ThrdTree.dep[index]->t3.pSet = pSet;
+	g_pThrdTree->dep[index]->t3.pSet = pSet;
 
 	memset(pSet, 0x00, sizeof(SET_T));
 
